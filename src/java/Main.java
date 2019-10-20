79a32e4fd3011dbdd5d2fd6ccadcb5876e3b8677
hjq562004191 <562004191@qq.com>
2019-10-20 22:43:19 +0800

    Java爬取豆瓣电影TOP250

--- src/java/Main.java
+++ src/java/Main.java
@@ -8,8 +8,8 @@ import util.PageDownLoadUtil;
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 250; i+=25) {
-            String url = "https://movie.douban.com/top250" + "?start="+i+"&amp;filter=";
+        for (int i = 0; i < 250; i += 25) {
+            String url = "https://movie.douban.com/top250" + "?start=" + i + "&amp;filter=";
             ProcessService ps = new ProcessServiceImpl();
             ps.process(PageDownLoadUtil.getPageContent(url));
         }
